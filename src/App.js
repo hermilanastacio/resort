10c857d56e4ec91cfd3c0ed21f920c5d81e31a84
hermilanastacio <hermilanastacio@pinion.ph>
2020-10-16 19:30:00 +0800

    - Test

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
 import Video from './components/Video/Video';
 import Dashboard from './components/Dashboard/Dashboard';
 import About from './components/About/About';
@@ -6,19 +7,31 @@ import Gallery from './components/Gallery/Gallery';
 import Rooms from './components/Rooms/Rooms';
 import Contact from './components/Contact/Contact';
 import Footer from './components/Footer/Footer';
+import Loading from './components/Loading/Loading';
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setIsLoading(false); 
+  },[]);
+
   return (
     <div className="App">
-      <Video/>
-      <div style={{position:"absolute"}}>
-        <Dashboard/>
-        <About/>
-        <Gallery/>
-        <Rooms/>
-        <Contact/>
-        <Footer/>
-      </div>
+      {isLoading
+        ? <Loading isLoading={isLoading}/>
+        : <React.Fragment>
+            <Video/>
+            <div style={{position:"absolute"}}>
+              <Dashboard/>
+              <About/>
+              <Gallery/>
+              <Rooms/>
+              <Contact/>
+              <Footer/>
+            </div>
+          </React.Fragment>
+      }
     </div>
   );
 }
---
+++ src/components/Loading/Loading.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Fade } from '@material-ui/core';
+
+const loadingImg = require('../../assets/loading.gif');
+
+const Loading = (props) => {
+  return(
+    <Fade in={props.isLoading} timeout={{exit:12000}}>
+      <div style={{backgroundColor:"#222", height:"100%", width:"100%", position:"absolute", display:"flex", alignItems:"center", justifyContent:"center"}}>
+        <img src={loadingImg} alt="loading gif" style={{height:120, width: 120}}/>
+      </div>
+    </Fade>
+  );
+}
+ 
+export default Loading;
