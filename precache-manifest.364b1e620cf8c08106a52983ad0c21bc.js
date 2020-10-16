346c30087ee65202f624e82f8db424f1795e7c6f
hermilanastacio <hermilanastacio@pinion.ph>
2020-10-16 19:34:21 +0800

    Updates

---
+++ precache-manifest.364b1e620cf8c08106a52983ad0c21bc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb3984123608ae7309343ed6092879f4",
+    "url": "/resort/index.html"
+  },
+  {
+    "revision": "3f4b5484d8743bcb8ac3",
+    "url": "/resort/static/css/main.d323262b.chunk.css"
+  },
+  {
+    "revision": "a5f14cefe09cbd3be928",
+    "url": "/resort/static/js/2.8cd6e4ae.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/resort/static/js/2.8cd6e4ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f4b5484d8743bcb8ac3",
+    "url": "/resort/static/js/main.06684234.chunk.js"
+  },
+  {
+    "revision": "186f18fd212240bbfce6",
+    "url": "/resort/static/js/runtime-main.612359e4.js"
+  },
+  {
+    "revision": "f48b9ed40043eec4030eb009dd2cf03d",
+    "url": "/resort/static/media/background.f48b9ed4.mp4"
+  },
+  {
+    "revision": "209c657f44203f836798a66eb0c473ec",
+    "url": "/resort/static/media/loading.209c657f.gif"
+  }
+]);
